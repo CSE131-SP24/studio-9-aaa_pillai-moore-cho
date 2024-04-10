4731578c585ad8dbf5bd48bd38729b3a75efbd3f
bc <bc@localhost>
2024-04-10 17:21:34 -0500

    studio 9

--- src/studio9/WordCount.java
+++ src/studio9/WordCount.java
@@ -30,9 +30,17 @@ public class WordCount {
 	 */
 	public static Map<String, Integer> countWords(List<String> words) {
 
-		// FIXME
-		throw new NotYetImplementedException();
-
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (int i = 0; i < words.size(); i++) {
+			if (map.containsKey(words.get(i))) {
+				int num = map.get(words.get(i));
+				map.put(words.get(i), num + 1);
+			}
+			else {
+				map.put(words.get(i), 1);
+			}
+		}
+		return map;
 	}
 	
 	public static void main(String[] args) {
